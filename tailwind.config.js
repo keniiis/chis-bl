21012e6077c68b6d2ffae7ac4188e1edf56b074e
keniiis <dakyom.dev@gmail.com>
2024-09-26 15:48:23 -0300

    new look

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,15 @@ module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'ffamily': '"Poppins", system-ui;',
+      },
+      fontSize: {
+        'texto-principal': 'clamp(2.5rem, 4vw + 1rem, 4.5rem)',
+        'texto-sec': 'clamp(14px, 1.5vw + 1rem, 39px)'
+      },
+    },
   },
   plugins: [],
 }
