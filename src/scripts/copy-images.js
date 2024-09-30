e31a14ce244f998d93456e47184e3dc30cc86596
keniiis <dakyom.dev@gmail.com>
2024-09-30 07:48:45 -0300

    script image

--- src/scripts/copy-images.js
+++ src/scripts/copy-images.js
@@ -1,5 +1,11 @@
-const fs = require('fs-extra');
-const path = require('path');
+import fs from 'fs-extra';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+// Para obtener correctamente __dirname en ES Modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const sourceDir = path.join(__dirname, '../public/images');
 const targetDir = path.join(__dirname, '../.vercel/output/static/images');
