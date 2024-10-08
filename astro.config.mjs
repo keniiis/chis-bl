65994585a05d79ea2b44b522d0a7043f99eb3de4
keniiis <dakyom.dev@gmail.com>
2024-10-08 17:46:53 -0300

    ver-jsn-prueba

--- astro.config.mjs
+++ astro.config.mjs
@@ -30,4 +30,11 @@ export default defineConfig({
   trailingSlash: 'never',
   output: 'static',
   adapter: vercelStatic(),
+  redirects: [
+    {
+      "source": "/:path*/",
+      "destination": "/:path*",
+      "permanent": false
+    }
+  ]
 });
