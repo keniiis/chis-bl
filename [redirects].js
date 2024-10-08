cfe58bad8b8d4273801cb3c5b7636c51be8a337a
keniiis <dakyom.dev@gmail.com>
2024-10-08 17:57:08 -0300

    ver-jsn-prueba1

---
+++ [redirects].js
@@ -0,0 +1,11 @@
+module.exports = {
+    async redirects() {
+      return [
+        {
+          source: '/:path+/$',
+          destination: '/:path',
+          permanent: true,
+        },
+      ]
+    },
+  }
